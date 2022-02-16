9618c3f1971f5f6bb715b66c7708e08d7007621f
DavidGentili <DavidGenntili@gmail.com>
2022-02-16 16:47:56 -0300

    Remove console.logs

--- database.js
+++ database.js
@@ -5,6 +5,5 @@ mongoose.connect('mongodb://localhost:27017/task-app')
     console.log('Database connected'); 
 })
 .catch( (e) => {
-    console.error(e);
     console.log('error to connect Database');
 });
