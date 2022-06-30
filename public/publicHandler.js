70404c1e2c87b0a483a345478feb0fdda212fc65
DavidGentili <DavidGenntili@gmail.com>
2022-06-30 15:48:35 -0300

    .

--- public/publicHandler.js
+++ public/publicHandler.js
@@ -47,7 +47,7 @@ const resourceHandler = async (req,res) => {
 
 
 const publicHandler = (req,res) => {
-    console.log(req.path[0] === 'js' ? req.path : '')
+    console.log(req.path[0] === 'js' ? req: '')
     const resource = ['css','js','img'];
     const path = req.path[0];
     if(resource.includes(path))
